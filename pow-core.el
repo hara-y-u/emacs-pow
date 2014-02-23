e00ac38ae77c5839111e4c60fb576d80961e8abb
yukihiro hara <yukihr@gmail.com>
2014-02-23 18:23:28 +0900

    inline document and fix error message fn

--- pow-core.el
+++ pow-core.el
@@ -37,12 +37,12 @@
   :group 'pow)
 
 (defcustom pow-url-format "http://%s.dev/"
-  "Url format of rack application registered on Pow. This is passed to format function with one argument: app name ."
+  "Url format of rack application registered on Pow. This is passed to format function with one argument: app-name."
   :type 'string
   :group 'pow)
 
 (defcustom pow-browse-url-function 'browse-url
-  "Function to browse app, takes app url."
+  "Function to browse app, takes an app-url."
   :type 'function
   :group 'pow)
 
@@ -64,6 +64,7 @@
         (t (pow-rack-project-root-for-dir (replace-regexp-in-string "[^/]+/?$" "" dir)))))
 
 (defun pow-same-file-p (&rest paths)
+  "Check if all passed paths are pointing to same path."
   (reduce #'(lambda (l r)
               (when (and
                      (stringp l) (stringp r)
@@ -74,9 +75,20 @@
           :start 0))
 
 (defun pow-message (format-string &rest args)
+  "Message utility. format-string and rest args are passed to `format',
+and then pass the output to `message'."
   (message (apply 'format (concat "Pow: " format-string) args)))
 
+(defun pow-error (format-string &rest args)
+  "Error utility. format-string and rest args are passed to `error'."
+  (apply 'error (concat "Pow: " format-string) args))
+
+(defun pow-user-error (format-string &rest args)
+  "User error utility. format-string and rest args are passed to `user-error'."
+  (apply 'user-error (concat "Pow: " format-string) args))
+
 (defun pow-filename-last-directory (path)
+  "Return the last entry of path."
   (string-match "\\/\\([^\\/]+\\)\\/?$" path)
   (match-string 1 path))
 
@@ -108,12 +120,19 @@
 (defstruct (pow-app
             (:constructor nil)
             (:constructor pow-app--inner-make))
+  "A structure abstructing pow app symlinks."
   (symlink-directory pow-symlink-directory)
   path
   (name "")
   )
 
 (defun make-pow-app (&rest options)
+  "Constructor for `pow-app'.
+
+options:
+`symlink-directory': symlink-directory of pow apps. (defalut: `pow-symlink-directory')
+`path': path to pow app project.
+`name': name of the pow app."
   (if (plist-get options :path)
       (setq options (plist-put options :path
                                (expand-file-name
@@ -125,26 +144,32 @@
     pow-app))
 
 (defun pow-app-set-name-with-path (app)
+  "Set app name to last entry of the `path'."
   (let* ((path (pow-app-path app))
          (name (pow-filename-last-directory path)))
     (setf (pow-app-name app) name)))
 
 (defun pow-app-set-name-default (app)
+  "Set app name to \"default\"."
   (setf (pow-app-name app) "default"))
 
 (defun pow-app-url (app)
+  "Gertter for url of the pow app."
   (format pow-url-format (pow-app-name app)))
 
 (defun pow-app-open (app)
+  "Open pow app url with function referenced by `pow-browse-url-function'."
   (let ((url (pow-app-url app)))
     (funcall pow-browse-url-function url)))
 
 (defun pow-app-symlink-path (app)
+  "Gertter for symlink's whole path of the pow app."
   (expand-file-name
    (pow-app-name app)
    (pow-app-symlink-directory app)))
 
 (defun pow-app-save (app)
+  "Save pow app as symlink in `symlink-directory'."
   (make-directory (pow-app-symlink-directory app) 'parents)
   (condition-case err
       (make-symbolic-link (pow-app-path app)
@@ -154,6 +179,7 @@
              (format "App \"%s\" already exists" (pow-app-name app))))))
 
 (defun pow-app-delete (app)
+  "Delete pow app symlink in `symlink-directory'."
   (condition-case err
       (delete-file (pow-app-symlink-path app))
     (file-error
@@ -163,6 +189,7 @@
                      pow-symlink-directory)))))
 
 (defun pow-app-restart (app)
+  "Flag pow app to restart on next request."
   (condition-case err
       (let* ((txt "restart.txt")
              (dir (expand-file-name "tmp" (pow-app-path app)))
@@ -176,6 +203,7 @@
                      path)))))
 
 (defun pow-app-load-all ()
+  "Load all pow apps in `symlink-directory'."
   (let* ((dir pow-symlink-directory)
          (symlinks (directory-files dir)))
     (remove-if
@@ -191,17 +219,22 @@
                     ) symlinks))))
 
 (defun pow-app-load-for-project (project-dir)
+  "Load pow app for `project-dir'."
   (remove-if #'(lambda (app)
                  (not (pow-same-file-p
                        (pow-app-path app) project-dir)))
              (pow-app-load-all)))
 
 (defun pow-app-load-for-dir (dir)
+  "Load pow app for `dir'.
+This function will find root project directory of `dir'
+and pass it to `pow-app-load-for-project'."
   (let ((project-path (pow-rack-project-root-for-dir dir)))
     (when project-path
       (pow-app-load-for-project project-path))))
 
 (defun pow-app-load-by-name (name)
+  "Load pow app for `name'."
   (car
    (remove-if #'(lambda (app)
                   (not (equal name (pow-app-name app))))
@@ -221,10 +254,12 @@
      "App not found")
 
 (defun pow-register-app (name path)
+  "Register pow app for `name' and `path'."
   (let ((app (make-pow-app :path path :name name)))
     (pow-app-save app)))
 
 (defmacro pow--with-name-or-app (name-or-app app &rest body)
+  "An macro receives `name-or-app' arg and call `body' certainly with `app'."
   (declare (indent 2))
   `(let (,app name)
      (if (pow-app-p ,name-or-app)
@@ -237,14 +272,17 @@
        (progn ,@body))))
 
 (defun pow-unregister-app (name-or-app)
+  "Unregister app specified by `name-or-app'."
   (pow--with-name-or-app name-or-app app
     (pow-app-delete app)))
 
 (defun pow-open-app (name-or-app)
+  "Open app specified by `name-or-app'."
   (pow--with-name-or-app name-or-app app
     (pow-app-open app)))
 
 (defun pow-restart-app (name-or-app)
+  "Restart app specified by `name-or-app'."
   (pow--with-name-or-app name-or-app app
     (pow-app-restart app)))
 
@@ -252,23 +290,27 @@
 ;; interactive function
 
 (defmacro pow-wrap-error-in-message (message-on-success &rest body)
+  "A macro translates errors to error message for interactive fns."
   (declare (indent 1))
   `(condition-case err
        (progn
          ,@body
          (unless (null ,message-on-success)
            (pow-message ,message-on-success)))
-     (error (pow-message (cdr err)))))
+     (error (pow-error (cdr err)))))
 
 (defmacro pow-with-rack-project-root (root-path &rest body)
+  "A macro verifies current-directory is in rack project,
+and call `body' with project\'s `root-path'."
   (declare (indent 1))
   `(let ((,root-path (pow-rack-project-root-for-dir default-directory)))
      (if ,root-path
          (progn ,@body)
-       (pow-message "Not in rack project"))))
+       (pow-user-error "Not in rack project"))))
 
 ;;;###autoload
 (defun pow-register-current-app (&optional name)
+  "Register current project as an pow app."
   (interactive)
   (pow-with-rack-project-root path
     (let (app
@@ -282,17 +324,22 @@
 
 ;;;###autoload
 (defun pow-register-current-app-as-default ()
+  "Registers current project as an pow app named \"default\"."
   (interactive)
   (pow-register-current-app "default"))
 
 (defmacro pow-with-current-apps (apps &rest body)
+  "Execute `body' with `apps' of current project."
   (declare (indent 1))
   `(let ((,apps (pow-app-load-for-dir default-directory)))
      (if (not (null ,apps))
          (progn ,@body)
-       (pow-message "App for current project was not found"))))
+       (pow-error "App for current project was not found"))))
 
 (defmacro pow-with-current-app (app &rest body)
+  "Execute `body' with an `app' of current project.
+If there is many app registered for the current project,
+shows prompt to choose one app from the apps."
   (declare (indent 1))
   `(pow-with-current-apps apps
      (if (eq (length apps) 1)
@@ -310,10 +357,11 @@
                      apps))))
          (if (not (null ,app))
              (progn ,@body)
-           (pow-message "App \"%s\" is not found" name))))))
+           (pow-error "App \"%s\" is not found" name))))))
 
 ;;;###autoload
 (defun pow-unregister-current-app ()
+  "Unregister a pow app for current project."
   (interactive)
   (pow-with-current-app app
     (pow-wrap-error-in-message
@@ -322,11 +370,13 @@
 
 ;;;###autoload
 (defun pow-open-current-app ()
+  "Open a pow app for current project."
   (interactive)
   (pow-with-current-app app (pow-open-app app)))
 
 ;;;###autoload
 (defun pow-restart-current-app ()
+  "Restart a pow app for current project."
   (interactive)
   (pow-with-current-app app
     (pow-wrap-error-in-message
