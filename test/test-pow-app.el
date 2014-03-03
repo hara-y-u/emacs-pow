690eb717e7d4e88a0ce782670633760523aae6aa
yukihiro hara <yukihr@gmail.com>
2014-03-03 22:39:11 +0900

    update test

--- test/test-pow-app.el
+++ test/test-pow-app.el
@@ -1,6 +1,6 @@
 ;;; test-pow-app.el --- unit test for pow-app
 
-;; Copyright (c) 2013  Free Software Foundation, Inc.
+;; Copyright (c) 2014 yukihiro hara
 
 ;; Author: yukihiro hara <yukihr@gmail.com>
 
@@ -21,6 +21,10 @@
 (require 'ert)
 (require 'pow-core)
 
+(defvar testdir "~/test-project")
+(make-directory testdir 'parent)
+(write-region "" nil (expand-file-name "config.ru" testdir) nil 'quiet)
+
 (ert-deftest make-pow-app ()
   (let ((app (make-pow-app :path "/home/huga/proj/hoge")))
     (should
@@ -38,13 +42,20 @@
   (let ((app (make-pow-app :path "/home/huga/proj/hoge")))
     (should (equal (pow-app-url app) "http://hoge.dev/"))))
 
-(ert-deftest pow-app-open ()
-  (let ((app (make-pow-app :path "/home/huga/proj/hoge")))
-    (pow-app-open app)
-    (should (y-or-n-p "App url opened correctly?"))))
+;; (ert-deftest pow-app-open ()
+;;   (let ((app (make-pow-app :path "/home/huga/proj/hoge")))
+;;     (pow-app-open app)
+;;     (should (y-or-n-p "App url opened correctly?"))))
+
+(ert-deftest pow-app-symlink-path ()
+  (let ((app (make-pow-app :path testdir
+                           :name "test-app")))
+    (should (pow-same-file-p
+             "~/.pow/test-app"
+             (pow-app-symlink-path app)))))
 
 (ert-deftest pow-app-save ()
-  (let* ((app (make-pow-app :path "~/Projects/test-project"))
+  (let* ((app (make-pow-app :path testdir))
          (symlink-path (expand-file-name
                         (pow-app-name app)
                         (pow-app-symlink-directory app))))
@@ -53,3 +64,6 @@
     (delete-file symlink-path)))
 
 (ert-run-tests-batch)
+
+(delete-directory testdir 'rec)
+(makunbound 'testdir)
