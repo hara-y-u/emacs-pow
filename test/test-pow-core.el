38378b3daa77188cbcf129e9eea0a7e4310a5043
yukihiro hara <yukihr@gmail.com>
2014-03-03 23:46:03 +0900

    update test

--- test/test-pow-core.el
+++ test/test-pow-core.el
@@ -21,10 +21,12 @@
 (require 'ert)
 (require 'pow-core)
 
-(defvar testdir "~/test-project")
-(make-directory testdir 'parent)
-(write-region "" nil (expand-file-name "config.ru" testdir) nil 'quiet)
+(defvar testdir "emacs-pow-test-project-20140303")
+(defvar testpath (expand-file-name testdir "~/"))
+(make-directory testpath 'parent)
+(write-region "" nil (expand-file-name "config.ru" testpath) nil 'quiet)
 
+;; pow-app
 (ert-deftest make-pow-app ()
   (let ((app (make-pow-app :path "/home/huga/proj/hoge")))
     (should
@@ -42,20 +44,34 @@
   (let ((app (make-pow-app :path "/home/huga/proj/hoge")))
     (should (equal (pow-app-url app) "http://hoge.dev/"))))
 
-;; (ert-deftest pow-app-open ()
-;;   (let ((app (make-pow-app :path "/home/huga/proj/hoge")))
-;;     (pow-app-open app)
-;;     (should (y-or-n-p "App url opened correctly?"))))
-
 (ert-deftest pow-app-symlink-path ()
-  (let ((app (make-pow-app :path testdir
+  (let ((app (make-pow-app :path testpath
                            :name "test-app")))
     (should (pow-same-file-p
              "~/.pow/test-app"
              (pow-app-symlink-path app)))))
 
+(ert-deftest pow-app-validate-with-no-args ()
+  (should-error
+   (let ((app (make-pow-app)))
+     (pow-app-validate app))))
+
+(ert-deftest pow-app-validate-with-invalid-path ()
+  (should-error
+   (let ((app (make-pow-app :path "/hoge/huga")))
+     (pow-app-validate app))))
+
+(ert-deftest pow-app-validate-with-invalid-name ()
+  (should-error
+   (let ((app (make-pow-app :path " foo bar ")))
+     (pow-app-validate app))))
+
+(ert-deftest pow-app-validate-with-valid-path ()
+  (let ((app (make-pow-app :path testpath)))
+    (pow-app-validate app)))
+
 (ert-deftest pow-app-save ()
-  (let* ((app (make-pow-app :path testdir))
+  (let* ((app (make-pow-app :path testpath))
          (symlink-path (expand-file-name
                         (pow-app-name app)
                         (pow-app-symlink-directory app))))
@@ -63,7 +79,31 @@
     (should (file-symlink-p symlink-path))
     (delete-file symlink-path)))
 
+(ert-deftest pow-app-delete ()
+  (let ((app (make-pow-app :path testpath)))
+    (pow-app-save app)
+    (should (file-symlink-p (pow-app-symlink-path app)))
+    (pow-app-delete app)
+    (should (not (file-exists-p (pow-app-symlink-path app))))))
+
+(ert-deftest pow-app-rename ()
+  (let ((app (make-pow-app :path testpath)))
+    (pow-app-save app)
+    (pow-app-rename app "emacs-pow-new-name")
+    (should (file-symlink-p
+             (expand-file-name "emacs-pow-new-name"
+                               pow-symlink-directory)))
+    (pow-app-delete app)))
+
+(ert-deftest pow-app-restart ()
+  (let* ((app (make-pow-app :path testpath))
+        (txt (expand-file-name "tmp/restart.txt"
+                               (pow-app-path app))))
+    (pow-app-restart app)
+    (should (file-exists-p txt))))
+
+
 (ert-run-tests-batch)
 
-(delete-directory testdir 'rec)
-(makunbound 'testdir)
+(delete-directory testpath 'rec)
+(makunbound 'testpath)
